de33476346a9a2c48d9ec50dc72440195d39cc56
Aravind Budaraju <budarajuaravind@gmail.com>
2025-09-14 15:28:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ console.log("MONGODB_URI RAW:", process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 10000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
